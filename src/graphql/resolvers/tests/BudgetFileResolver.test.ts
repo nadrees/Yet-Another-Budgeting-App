5685bf77febf2df3baab37be47084166595dfb18
nadrees <ndrees@noreply.com>
2020-02-21 15:07:36 -0600

    Finished createBudget tests

--- src/graphql/resolvers/tests/BudgetFileResolver.test.ts
+++ src/graphql/resolvers/tests/BudgetFileResolver.test.ts
@@ -1,7 +1,10 @@
 import { BudgetFileResolver, getDbFolder } from "../BudgetFileResolver";
 
+import { Budget } from "../../../db/entity/Budget";
 import { USER_DATA_PATH } from "../../../tests/setupEnvironment";
 import fs from "fs";
+import { getRepository } from "typeorm";
+import { initConnect } from "../../../db/ConnectionUtils";
 import path from "path";
 import util from "util";
 
@@ -10,9 +13,14 @@ const EXPECTED_BUDGET_FOLDER_PATH = path.join(USER_DATA_PATH, "budgets");
 const existsSpy = jest.spyOn(fs, "exists");
 
 describe("BudgetFileResolver", () => {
+  let resolver: BudgetFileResolver;
+
+  beforeEach(() => {
+    resolver = new BudgetFileResolver();
+  });
+
   describe("budgetFiles", () => {
     it("should form the correct db folder path", async () => {
-      const resolver = new BudgetFileResolver();
       await resolver.budgetFiles();
 
       expect(existsSpy).toHaveBeenCalledWith(
@@ -22,7 +30,6 @@ describe("BudgetFileResolver", () => {
     });
 
     it("should create the folder if it does not exist", async () => {
-      const resolver = new BudgetFileResolver();
       await resolver.budgetFiles();
 
       const exists = fs.existsSync(EXPECTED_BUDGET_FOLDER_PATH);
@@ -30,8 +37,6 @@ describe("BudgetFileResolver", () => {
     });
 
     describe("should return the correct set of file", () => {
-      const resolver = new BudgetFileResolver();
-
       test("when there are no files", async () => {
         const files = await resolver.budgetFiles();
         expect(files.length).toBe(0);
@@ -63,6 +68,35 @@ describe("BudgetFileResolver", () => {
         const files = await resolver.budgetFiles();
 
         expect(files.length).toBe(2);
+      });
+    });
+  });
+
+  describe("createBudget", () => {
+    describe("when creating a new budget", () => {
+      it("should create a new .yaba file", async () => {
+        const result = await resolver.createBudget({ name: "test" });
+        expect(result.budgetFile?.path).not.toBeNull();
+        expect(result.budgetFile?.path).toContain("test");
+      });
+
+      it("should sanitize the file name", async () => {
+        const result = await resolver.createBudget({ name: "test!?&*" });
+        expect(result.budgetFile?.path).not.toBeNull();
+        expect(result.budgetFile?.path).toContain("test");
+      });
+
+      it("should insert the budget name in to the db", async () => {
+        const result = await resolver.createBudget({ name: "test" });
+        expect(result.budgetFile?.path).not.toBeNull();
+
+        await initConnect(result.budgetFile?.path ?? "");
+
+        const budgets = await getRepository(Budget).find();
+        expect(budgets.length).toEqual(1);
+
+        expect(budgets[0].name).toBe("test");
+        expect(budgets[0].id).not.toBeNull();
       });
     });
   });
--- src/tests/setupEnvironment.ts
+++ src/tests/setupEnvironment.ts
@@ -2,12 +2,20 @@ export const USER_DATA_PATH = "test";
 
 import "reflect-metadata";
 
+import { getConnectionManager } from "typeorm";
 import rimraf from "rimraf";
 
 jest.mock("electron", () => ({
   app: { getPath: jest.fn().mockReturnValue(USER_DATA_PATH) }
 }));
 
-afterEach(() => {
+afterEach(async () => {
+  // close any open connections
+  await Promise.all(
+    getConnectionManager()
+      .connections.filter(connection => connection.isConnected)
+      .map(async connection => await connection.close())
+  );
+
   rimraf.sync(USER_DATA_PATH);
 });
